fd73c9683cf2c9a90ed58891930811e59729f7f1
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-02-25 16:20:13 +0600

    добавил пару классов в models.py!

--- list/webapp/models.py
+++ list/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class list(models.Model):
+    description = models.CharField(max_length=120, null=False, blank=False)
+    status = models.TextField(max_length=3000, null=False, blank=False)
+    updated_at = models.DateField(auto_now=True)
+
+
+    class Meta:
+        db_table = 'Lists'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return f'{self.id}. {self.status}: {self.description}'
