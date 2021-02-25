075ff78d99fb3471a101f3c0ee5681b418be9452
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-02-25 16:42:36 +0600

    создал list.html  и создал метод list_view в webapp.view !

--- list/webapp/views.py
+++ list/webapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from webapp.models import List
+
+def list_view(request):
+    list = List.objects.all()
+    return render(request, 'list.html', context={'lists': list})
+
