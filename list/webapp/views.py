c309097d4574a8d5baf25dd628eff8900ba68ee2
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-03-01 15:28:58 +0600

    в предыдушем комите добавление было не до конца реализована! испраивл ошибку и сделал новый комит!

--- list/webapp/views.py
+++ list/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.urls import reverse
 
@@ -20,16 +20,19 @@ def tasks_create_view(request):
     if request.method == "GET":
         return render(request, 'tasks_add_view.html', {'status': status_choices})
     elif request.method == "POST":
-        description = request.POST.get("description")
+        description = request.POST.get('description')
+        detailed_description = request.POST.get('detailed_description')
         status = request.POST.get('status')
+
         updated_at = request.POST.get('updated_at')
         if updated_at == '':
             updated_at = None
 
         list = List.objects.create(
             description=description,
+            detailed_description=detailed_description,
             status=status,
             updated_at=updated_at
         )
-        uri = reverse('task', kwargs={"id": list.id})
-        return HttpResponseRedirect(uri)
+
+        return redirect('task', id=list.id)
