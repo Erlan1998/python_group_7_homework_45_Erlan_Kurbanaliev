575c95535502661d8a3dfc14b7e502aba09aa0d8
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-03-03 17:41:12 +0600

    реализовал метод редактирования с использованием ранее созданного шаблона!

--- list/webapp/views.py
+++ list/webapp/views.py
@@ -24,9 +24,9 @@ def tasks_create_view(request):
         if form.is_valid():
             list = List.objects.create(
                 description=form.cleaned_data.get('description'),
-                detailed_description = form.cleaned_data.get('detailed_description'),
-                status = form.cleaned_data.get('status'),
-                updated_at = form.cleaned_data.get('updated_at')
+                detailed_description=form.cleaned_data.get('detailed_description'),
+                status=form.cleaned_data.get('status'),
+                updated_at=form.cleaned_data.get('updated_at')
             )
             return redirect('task', id=list.id)
         return render(request, 'tasks_add_view.html', context={'form': form})
@@ -34,14 +34,23 @@ def tasks_create_view(request):
 def list_update_view(request, id):
     list = get_object_or_404(List, id=id)
     if request.method == 'GET':
-        return render(request, 'list_update.html', context={'list': list, "status": status_choices})
+        form = ListForm(initial={
+            'description': list.description,
+            'detailed_description': list.detailed_description,
+            'status': list.status,
+            'updated_at': list.updated_at
+        })
+        return render(request, 'list_update.html', context={'form': form, 'list': list, "status": status_choices})
     elif request.method == 'POST':
-        list.description = request.POST.get('description')
-        list.detailed_description = request.POST.get('detailed_description')
-        list.status = request.POST.get('status')
-        list.updated_at = request.POST.get('updated_at')
-        list.save()
-        return redirect('task', id=list.id)
+        form = ListForm(data=request.POST)
+        if form.is_valid():
+            list.description = request.POST.get('description')
+            list.detailed_description = request.POST.get('detailed_description')
+            list.status = request.POST.get('status')
+            list.updated_at = request.POST.get('updated_at')
+            list.save()
+            return redirect('task', id=list.id)
+        return render(request, 'tasks_add_view.html', context={'form': form, 'list': list})
 
 def list_delete_view(request, id):
     list = get_object_or_404(List, id=id)
