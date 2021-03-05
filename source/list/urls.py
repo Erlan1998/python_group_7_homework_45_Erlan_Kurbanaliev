d8825668946dfaa1171a7a9e2c30ec21b57006eb
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-03-05 17:32:07 +0600

    Реализовал главную страницу!

--- source/list/urls.py
+++ source/list/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import products_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', products_view, name='index_all')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from webapp.models import Product, category_choices
+from webapp.forms import ProductForm
+
+
+def products_view(request):
+    product = Product.objects.all()
+    return render(request, 'index.html', context={'products': product})
