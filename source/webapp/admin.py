6e9044cc65c598f11164d6cff5e526e7ec2b3763
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-03-05 15:41:56 +0600

    Реализовал админку!

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from webapp.models import Product
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category',  'price']
+    list_filter = ['category']
+    search_fields = ['category']
+    fields = ['id', 'name', 'category', 'price', 'balance']
+    readonly_fields = ['id']
+
+admin.site.register(Product, ProductAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+category_choices = [('other', 'Разное'), ('sports', 'Спортивне'),  ('Classic', 'Классические'), ('For home', 'Для дома'), ('for study', 'Для учебы')]
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(max_length=2000, null=False, blank=True)
+    category = models.CharField(max_length=100, null=False, blank=False, choices=category_choices)
+    balance = models.IntegerField(null=False, blank=False)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False)
+
+    class Meta:
+        db_table = 'Lists'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'{self.id}. {self.category}: {self.name}'
