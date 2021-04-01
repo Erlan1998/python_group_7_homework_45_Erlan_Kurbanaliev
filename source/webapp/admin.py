ebe1d95ad127f104fbfd44d41a9071bb5d30e3aa
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-04-01 19:57:25 +0600

    переписал категорию в модельке и создал его в админке применил миграцию и изменил фикстуры!

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from webapp.models import Product
+from webapp.models import Product, Categories
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ['id']
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Categories)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
-# Create your models here.
-category_choices = [('other', 'Разное'), ('sports', 'Спортивне'),  ('Classic', 'Классические'), ('For home', 'Для дома'), ('for study', 'Для учебы')]
+
+# category_choices = [('other', 'Разное'), ('sports', 'Спортивне'),  ('Classic', 'Классические'), ('For home', 'Для дома'), ('for study', 'Для учебы')]
 class Product(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField(max_length=2000, null=False, blank=True)
-    category = models.CharField(max_length=100, null=False, blank=False, default='other', choices=category_choices)
+    category = models.ForeignKey('webapp.Categories', related_name='Products', on_delete=models.PROTECT)
     quantity = models.IntegerField(null=False, blank=False, validators=[MinValueValidator(0)])
     price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False)
 
@@ -16,4 +16,10 @@ class Product(models.Model):
         verbose_name_plural = 'Продукты'
 
     def __str__(self):
-        return f'{self.id}. {self.category}: {self.name}'
+        return f'{self.id}. {self.name}'
+
+class Categories(models.Model):
+    category = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.category}'
