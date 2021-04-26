0bcd7e1377fd858db323c5e4d23238f423f44d10
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-04-26 18:40:24 +0600

    Реализовал страничку просмотра всех заказов пользователя!

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import register_view, MyLogoutView
+from accounts.views import register_view, MyLogoutView, BookingView
 from django.contrib.auth.views import LoginView
 
 
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', MyLogoutView.as_view(), name='logout'),
     path('registration/', register_view, name='register'),
-    # path('index/', UserView.as_view(), name='index_accounts'),
+    path('booking/', BookingView.as_view(), name='booking'),
     # path('<int:id>/', UserDetailView.as_view(), name='all_accounts'),
     # path('profile/', UserUpdateView.as_view(), name='update_user'),
     # path('profile/change-password', UserChangePasswordView.as_view(), name='change_password'),
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import login
 from django.contrib.auth.views import LogoutView
-
+from django.db.models import Q
+from django.views.generic import ListView
 from accounts.forms import UserRegisterForm
 from django.shortcuts import render, redirect
-
-from webapp.models import Basket, Product
+from django.utils.http import urlencode
+from webapp.forms import SearchForm
+from webapp.models import Basket, Product, Booking
 
 
 def register_view(request, *args, **kwargs):
@@ -35,3 +37,37 @@ class MyLogoutView(LogoutView):
                 basket.delete()
         return super().dispatch(request, *args, **kwargs)
 
+
+class BookingView(ListView):
+    template_name = 'booking_view.html'
+    model = Booking
+    context_object_name = 'bookings'
+    ordering = ('name',)
+
+    def get(self, request, **kwargs):
+        self.form = SearchForm(request.GET)
+        self.search_data = self.get_search_data()
+        return super(BookingView, self).get(request, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(user__id=self.request.user.id)
+        if self.search_data:
+            queryset = queryset.filter(
+                Q(name__icontains=self.search_data)
+            )
+        return queryset
+
+    def get_search_data(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search_value']
+        return None
+
+    def get_context_data(self, **kwargs):
+        kwargs = super().get_context_data(**kwargs)
+        kwargs['search_form'] = self.form
+
+        if self.search_data:
+            kwargs['query'] = urlencode({'search_value': self.search_data})
+
+        return kwargs
